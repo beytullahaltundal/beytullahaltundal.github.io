4eaec6dea1cf6265954f326b3f440b2bcf7f5dce
Beytullah Altundal <190688779+beytullahaltundal@users.noreply.github.com>
2025-02-08 17:06:45 +0300

    Update home.js

--- home.js
+++ home.js
@@ -1,15 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let codeOutput = document.querySelector('.code-output');
-    let codeText = `> Hello, welcome to Spektrumhex!\n> Initiating system...\n> Loading data...`;
-    let i = 0;
-
-    function typeCode() {
-        if (i < codeText.length) {
-            codeOutput.textContent += codeText.charAt(i);
-            i++;
-            setTimeout(typeCode, 50);
-        }
-    }
-
-    typeCode();
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Home page loaded!");
 });
