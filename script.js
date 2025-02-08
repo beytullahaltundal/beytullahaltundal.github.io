52701501aef3e64309731d9ae7bcfcf10f1e55ff
Beytullah Altundal <190688779+beytullahaltundal@users.noreply.github.com>
2025-02-08 16:43:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let codeOutput = document.querySelector('.code-output');
-    let codeText = `> Hello, welcome to Spektrumhex!\n> Loading Projects...\n> System Initialized...`;
-    let i = 0;
 
-    function typeCode() {
-        if (i < codeText.length) {
-            codeOutput.textContent += codeText.charAt(i);
-            i++;
-            setTimeout(typeCode, 50);
-        }
-    }
-
-    typeCode();
-});
