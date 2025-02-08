b4a635e2154a569263210e1250c98a1a3ca89710
Beytullah Altundal <190688779+beytullahaltundal@users.noreply.github.com>
2025-02-08 15:46:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const header = document.querySelector("header h1");
-    setInterval(() => {
-        header.style.color = header.style.color === "cyan" ? "white" : "cyan";
-    }, 1000);
-});
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".nav-tabs li");
+  const tabContent = document.querySelectorAll(".tab");
 
+  tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+      // Tüm sekmelerden ve içeriklerden aktif sınıfını kaldır
+      tabs.forEach(item => item.classList.remove("active"));
+      tabContent.forEach(content => content.classList.remove("active"));
+      
+      // Tıklanan sekmeye ve ilgili içeriğe aktif sınıfı ekle
+      tab.classList.add("active");
+      document.getElementById(tab.getAttribute("data-tab")).classList.add("active");
+    });
+  });
+});
