79e2a64388d768d4f9710a79e63c6fd2d5a341f7
Beytullah Altundal <190688779+beytullahaltundal@users.noreply.github.com>
2025-02-08 16:39:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Başlık animasyonu
-    let header = document.querySelector("header h1");
-    header.style.opacity = "0";
-    header.style.transform = "translateY(-20px)";
+document.addEventListener('DOMContentLoaded', function() {
+    let codeOutput = document.querySelector('.code-output');
+    let codeText = `> Hello, welcome to Spektrumhex!\n> Loading Projects...\n> System Initialized...`;
+    let i = 0;
 
-    setTimeout(() => {
-        header.style.transition = "opacity 1.5s, transform 1.5s";
-        header.style.opacity = "1";
-        header.style.transform = "translateY(0)";
-    }, 500);
+    function typeCode() {
+        if (i < codeText.length) {
+            codeOutput.textContent += codeText.charAt(i);
+            i++;
+            setTimeout(typeCode, 50);
+        }
+    }
 
-    // Resimlere hover efekti ekleme
-    let images = document.querySelectorAll(".content img");
-
-    images.forEach(img => {
-        img.addEventListener("mouseover", () => {
-            img.style.transform = "scale(1.1)";
-            img.style.transition = "transform 0.5s";
-        });
-
-        img.addEventListener("mouseout", () => {
-            img.style.transform = "scale(1)";
-        });
-    });
+    typeCode();
 });
-
