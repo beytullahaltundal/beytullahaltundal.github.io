4147c61fc431f448eff3c12d6b5734b80ff3235b
Beytullah Altundal <190688779+beytullahaltundal@users.noreply.github.com>
2025-02-08 15:43:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const header = document.querySelector("header h1");
+    setInterval(() => {
+        header.style.color = header.style.color === "cyan" ? "white" : "cyan";
+    }, 1000);
+});
 
